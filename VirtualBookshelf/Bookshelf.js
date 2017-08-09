d6fc70f699d1b5ba425b21076cf0df437f6063e6
casey fritsch <caseyfritsch@gmail.com>
2017-08-09 11:46:36 -0700

    generated users migration & model

---
+++ VirtualBookshelf/Bookshelf.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Book from './book';
+import Canvas from 'react-native-canvas';
+
+export default class App extends React.Component {
+
+  handleCanvas (canvas) {
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(0, 0, 100, 100);
+    let img = new Image();
+    img.src = "./img/TheHobbit_cover.jpg";
+    ctx.drawImage(img, 0, 0, 50, 325);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>at the Bookshelf</Text>
+        <Book />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- VirtualBookshelf/book.js
+++ VirtualBookshelf/book.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Bookshelf from './Bookshelf';
 
 export default class App extends React.Component {
-  render() {
+showBook () {
     return (
       <View>
-        <Text>blargggg.</Text>
-        <Bookshelf />
+        <Image
+          source={require('./img/TheHobbit_cover.jpg')}
+          style={styles.book}
+        />
+      </View>
+    );
+  }
+
+  render() {
+
+    return (
+      <View>
+        <Text>at the Book</Text>
+        {this.showBook()}
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,8 +30,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  book: {
-    width: 100,
-    height: 100
-  }
+
 });
