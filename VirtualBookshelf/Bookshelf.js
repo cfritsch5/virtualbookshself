9c4ae78de19b850930d5fffac44a92943e6e2b96
casey fritsch <caseyfritsch@gmail.com>
2017-08-09 17:08:35 -0700

    delete faulty rails project

--- VirtualBookshelf/Bookshelf.js
+++ VirtualBookshelf/Bookshelf.js
@@ -8,17 +8,16 @@ export default class App extends React.Component {
   handleCanvas (canvas) {
     const ctx = canvas.getContext('2d');
     ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
-    let img = new Image();
-    img.src = "./img/TheHobbit_cover.jpg";
-    ctx.drawImage(img, 0, 0, 50, 325);
+    ctx.fillRect(0, 0,10,10, 100, 100);
+    // let img = new Image();
+    // img.src = "./img/TheHobbit_cover.jpg";
+    // ctx.drawImage(img, 0, 0, 50, 325);
   }
 
   render() {
     return (
       <View>
         <Text>at the Bookshelf</Text>
-        <Book />
       </View>
     );
   }
